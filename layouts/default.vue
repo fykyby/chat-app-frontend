0416981dc3bea84e5030629dfdab9e440285d557
fykyby <95149002+fykyby@users.noreply.github.com>
2024-06-19 02:39:50 +0200

    Remove max width on page content

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="flex h-full">
     <Sidebar />
-    <main
-      class="mx-auto flex w-full max-w-5xl flex-col gap-2 p-2 sm:gap-4 sm:p-4"
-    >
+    <main class="mx-auto flex w-full flex-col gap-2 p-2 lg:gap-4 lg:p-4">
       <slot />
     </main>
   </div>
