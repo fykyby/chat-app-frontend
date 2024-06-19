3df2742d69632b833f9774403a2f63b82abedc04
fykyby <95149002+fykyby@users.noreply.github.com>
2024-06-19 03:44:31 +0200

    Chat page ui

--- pages/chats/[id].vue
+++ pages/chats/[id].vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import type { ApiResponse, Chat, Message } from "~/lib/types";
-import { ArrowLeft } from "lucide-vue-next";
+import { ArrowLeft, Send } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Input } from "@/components/ui/input";
 
 interface ChatResponse extends ApiResponse {
   data: {
@@ -14,6 +15,7 @@ interface ChatResponse extends ApiResponse {
 
 const config = useRuntimeConfig();
 const route = useRoute();
+const user = useUser();
 
 const headers = useRequestHeaders(["cookie"]);
 const { data, pending } = await useLazyFetch<ChatResponse>(
@@ -24,6 +26,16 @@ const { data, pending } = await useLazyFetch<ChatResponse>(
     ignoreResponseError: true,
   },
 );
+
+const message = ref("");
+
+async function sendMessage(e: Event) {
+  e.preventDefault();
+
+  console.log(message.value);
+
+  message.value = "";
+}
 </script>
 
 <template>
@@ -54,13 +66,46 @@ const { data, pending } = await useLazyFetch<ChatResponse>(
           </span>
         </div>
         <Separator />
-        <ScrollArea class="h-full flex-grow">
-          <ul>
-            <li v-for="message in data?.data.messages">
-              <div>{{ message.content }}</div>
+        <ScrollArea class="h-full">
+          <ul
+            class="flex h-full flex-grow flex-col-reverse gap-2 py-2 lg:gap-4 lg:py-4"
+          >
+            <li
+              v-for="message in data?.data.messages"
+              class="flex w-full items-end gap-2"
+              :class="{ 'flex-row-reverse': user?.id === message.user.id }"
+            >
+              <Avatar v-if="user?.id !== message.user.id" class="h-8 w-8">
+                <AvatarImage :src="message.user.avatar" alt="" />
+                <AvatarFallback>{{ message.user.name[0] }}</AvatarFallback>
+              </Avatar>
+              <div>
+                <div
+                  v-if="user?.id !== message.user.id"
+                  class="mb-0.5 px-1 text-sm text-muted-foreground"
+                  :class="{ 'text-right': user?.id === message.user.id }"
+                >
+                  {{ message.user.name }}
+                </div>
+                <div
+                  class="w-fit rounded-lg border p-2 lg:leading-7"
+                  :class="{
+                    'bg-muted': user?.id === message.user.id,
+                    'text-foreground': user?.id === message.user.id,
+                  }"
+                >
+                  {{ message.content }}
+                </div>
+              </div>
             </li>
           </ul>
         </ScrollArea>
+        <form class="flex gap-1" @submit="sendMessage">
+          <Input v-model="message" autofocus placeholder="Type a message..." />
+          <Button type="submit" size="icon" variant="outline">
+            <Send />
+          </Button>
+        </form>
       </div>
     </div>
   </div>
