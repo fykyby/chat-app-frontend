3abd261ec1c9953b48ea69993a46baed34e8e486
fykyby <95149002+fykyby@users.noreply.github.com>
2024-06-20 23:58:36 +0200

    Add pagination & infinite scroll to NewChatDialog user list

--- components/NewChatDialog.vue
+++ components/NewChatDialog.vue
@@ -1,107 +1,105 @@
 <script setup lang="ts">
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { Plus } from "lucide-vue-next";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import { Search } from "lucide-vue-next";
 import type { ApiResponse, Chat, User } from "~/lib/types";
+import { useInfiniteScroll } from "@vueuse/core";
 
 interface UsersResponse extends ApiResponse {
   data: {
     users: User[];
     hasMore: boolean;
-  } | null;
+  };
 }
 
 interface ChatResponse extends ApiResponse {
   data: Chat;
 }
 
+const SCROLL_OFFSET = 50;
+
 const config = useRuntimeConfig();
 
+const scrollElement_ = ref<HTMLElement | null>(null);
+const scrollElement = computed(
+  (): HTMLElement | null =>
+    scrollElement_.value?.parentElement?.parentElement ?? null,
+);
+const page = ref(1);
+const results = ref<User[]>([]);
+const query = ref("");
 const open = ref(false);
 const searchTimeout = ref<NodeJS.Timeout>();
-const query = ref("");
-const pending = ref(false);
-const data = ref<UsersResponse>();
-// const results = ref<User[]>([]);
 
-// TODO: Implement pages
-
-async function search(query: string) {
-  pending.value = true;
-
-  const response = await $fetch<UsersResponse>(
-    config.public.apiUrl + "/users",
-    {
-      query: { q: query, page: 1 },
-      method: "GET",
-      credentials: "include",
-      ignoreResponseError: true,
-      async onRequestError() {
-        data.value = {
-          ok: false,
-          message: config.public.errorMessage,
-          data: null,
-        };
-
-        pending.value = false;
-      },
+const { data, status, refresh } = await useLazyFetch<UsersResponse>(
+  config.public.apiUrl + "/users",
+  {
+    params: {
+      q: query,
+      page: page,
     },
-  );
+    headers: useRequestHeaders(["cookie"]),
+    credentials: "include",
+    ignoreResponseError: true,
+    server: false,
+    watch: false,
+    immediate: false,
+    async onResponse(c) {
+      results.value = [...results.value, ...c.response._data.data.users];
+    },
+  },
+);
 
-  data.value = response;
-
-  pending.value = false;
-}
+useInfiniteScroll(
+  scrollElement,
+  async () => {
+    if (data.value?.data.hasMore) {
+      page.value += 1;
+      refresh();
+    }
+  },
+  {
+    offset: {
+      bottom: SCROLL_OFFSET,
+    },
+    interval: 500,
+  },
+);
 
 async function startChat(userID: number) {
-  pending.value = true;
-
   const response = await $fetch<ChatResponse>(config.public.apiUrl + "/chats", {
     method: "POST",
     credentials: "include",
     ignoreResponseError: true,
     body: JSON.stringify({ recipientID: userID }),
     async onRequestError() {
-      data.value = {
-        ok: false,
-        message: config.public.errorMessage,
-        data: null,
-      };
-
-      pending.value = false;
+      if (!data.value) return;
+      (data.value.ok = false),
+        (data.value.message = config.public.errorMessage);
     },
   });
 
-  pending.value = false;
-
   if (response.ok) {
-    await refreshNuxtData("chats");
-    await navigateTo("/chats/" + response.data.id);
     open.value = false;
+    await navigateTo("/chats/" + response.data.id);
+    await refreshNuxtData("chats");
   }
 }
 
-function openStateChanged(open: boolean) {
+async function openStateChanged(open: boolean) {
   if (!open) {
     clearTimeout(searchTimeout.value);
   } else {
-    search("");
+    if (data.value) return;
+    await refresh();
   }
 }
 
-watch(query, (q) => {
+watch(query, () => {
   clearTimeout(searchTimeout.value);
-  searchTimeout.value = setTimeout(() => {
-    search(q);
+  searchTimeout.value = setTimeout(async () => {
+    results.value = [];
+    page.value = 1;
+    await refresh();
   }, 500);
 });
 
@@ -134,20 +132,19 @@ onUnmounted(() => {
             <Search class="size-5 text-muted-foreground" />
           </span>
         </div>
-        <Loading v-if="pending" class="mx-auto mt-2" />
         <AlertError
           class="mt-2"
-          v-else-if="data?.ok === false"
+          v-if="data?.ok === false"
           :message="data.message"
         />
         <AlertInfo
           class="mt-2"
-          v-else-if="data?.data?.users.length === 0"
+          v-else-if="results.length === 0"
           :message="config.public.noResultsMessage"
         />
         <ScrollArea v-else class="h-full max-h-[40dvh] p-2">
-          <ul class="flex flex-col gap-2">
-            <li v-for="user in data?.data?.users" :key="user.id">
+          <ul ref="scrollElement_" class="flex flex-col gap-2">
+            <li v-for="user in results" :key="user.id">
               <UserButton
                 @click="startChat(user.id)"
                 :name="user.name"
@@ -155,6 +152,9 @@ onUnmounted(() => {
               />
             </li>
           </ul>
+          <div v-if="status === 'pending'">
+            <Loading class="mx-auto my-2" />
+          </div>
         </ScrollArea>
       </div>
     </DialogContent>
